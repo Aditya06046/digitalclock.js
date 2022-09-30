e4f9e4c18e1268d1d65c57bbbcbc8a02f73f9ea6
Aditya06046 <109231656+Aditya06046@users.noreply.github.com>
2022-09-30 10:07:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import DigitalClock from "./digital_clock";
+export default function App() {
+  return (
+    <div style={{ width: "90%", margin: "0 auto" }}>
+      <DigitalClock />
+    </div>
+  );
+}
